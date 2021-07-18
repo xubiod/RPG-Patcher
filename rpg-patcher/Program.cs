f86752f1edd2a8993a7222c74755d9875551c25b
xubiod <xubiod.mail@gmail.com>
2021-07-18 12:13:12 -0400

    moved around menubar and made keyboard shortcuts make more sense

--- rpg-patcher/Program.cs
+++ rpg-patcher/Program.cs
@@ -39,43 +39,42 @@ namespace rpg_patcher
                         StaticWindows.About about = new StaticWindows.About();
                         Application.Run(about._window);
                     }),
-                    new MenuItem ("_Focus on main window", "", () => {
-                        //Application.Top.SetFocus(StaticWindows.Main._window);
-                        StaticWindows.Main._window.SetFocus();
+                    new MenuItem ("_Load", "", () => {
+                        Functions.FileDialog.CreateOpenDialog("Project", "Pick a project", new string[] { "rgssad", "rgss2a", "rgss3a" }, () => UpdateElements());
                     }),
+                    new MenuItem ("_Settings", "", () => {
+                        StaticWindows.Settings settings = new StaticWindows.Settings();
+                        Application.Run(settings._window);
+                    }),
+                    //new MenuItem ("_Focus on main window", "", () => {
+                    //    //Application.Top.SetFocus(StaticWindows.Main._window);
+                    //    StaticWindows.Main._window.SetFocus();
+                    //}),
                     new MenuItem ("_Quit", "", () => {
                         Application.RequestStop();
                     })
                 }),
 
                 new MenuBarItem ("_Project", new MenuItem [] {
-                    new MenuItem ("_Load (DO FIRST)", "", () => {
-                        Functions.FileDialog.CreateOpenDialog("Project", "Pick a project", new string[] { "rgssad", "rgss2a", "rgss3a" }, () => UpdateElements());
-                    }),
-                    null,
-                    new MenuItem ("_Create project files with loaded version", "", () => {
+                    new MenuItem ("Create _project files with loaded version", "", () => {
                         Functions.Project.MakeProject();
                     }),
                     null,
-                    new MenuItem ("_Create XP project files", "", () => {
+                    new MenuItem ("Create _XP project files", "", () => {
                         Functions.Project.MakeProjectIndeterministic(RPGMakerVersion.Xp);
                     }),
-                    new MenuItem ("_Create VX project files", "", () => {
+                    new MenuItem ("Create _VX project files", "", () => {
                         Functions.Project.MakeProjectIndeterministic(RPGMakerVersion.Vx);
                     }),
-                    new MenuItem ("_Create VX Ace project files", "", () => {
+                    new MenuItem ("_Create VX _Ace project files", "", () => {
                         Functions.Project.MakeProjectIndeterministic(RPGMakerVersion.VxAce);
                     }),
-                    new MenuItem ("_Create MV project files", "", () => {
+                    new MenuItem ("Create _MV project files", "", () => {
                         Functions.Project.MakeMVProject();
                     })
                 }),
 
                 new MenuBarItem ("_Extracting", new MenuItem [] {
-                    new MenuItem ("_Load (DO FIRST)", "", () => {
-                        Functions.FileDialog.CreateOpenDialog("Project", "Pick a project", new string[] { "rgssad", "rgss2a", "rgss3a" }, () => UpdateElements());
-                    }),
-                    null,
                     new MenuItem ("_Copy game files from project directory", "", () => {
                         Functions.Extract.CopyGameFiles();
                     }),
@@ -83,20 +82,20 @@ namespace rpg_patcher
                         Functions.Extract.GetAllFiles();
                     }),
                     null,
-                    new MenuItem ("_Extract single file", "", () => {
+                    new MenuItem ("Extract _single file", "", () => {
                         Functions.Operation.ExecuteIfProjectSelected(() => {
                             StaticWindows.ExportOneFile oneFile = new StaticWindows.ExportOneFile();
                             Application.Run(oneFile._window);
                         });
                     }),
-                    new MenuItem ("_Extract everything", "", () => {
+                    new MenuItem ("Extract _everything", "", () => {
                         Functions.FileDialog.CreateSaveDialog("Save to...", "Pick a place", new string[] { "" }, () => Functions.Operation.ExecuteIfProjectSelected(() => Functions.Extract.ExtractAllFiles()));
                     }),
-                    new MenuItem ("_Extract everything + project files", "", () => {
+                    new MenuItem ("Extract everything + _project files", "", () => {
                         Functions.FileDialog.CreateSaveDialog("Save to...", "Pick a place", new string[] { "" }, () => Functions.Operation.ExecuteIfProjectSelected(() => Functions.Extract.ExtractAllFiles(true)));
                         Functions.Project.MakeProjectWithSavePath();
                     }),
-                    new MenuItem ("_Extract everything + project files + game", "", () => {
+                    new MenuItem ("Extract everything + project files + _game", "", () => {
                         Functions.FileDialog.CreateSaveDialog("Save to...", "Pick a place", new string[] { "" }, () => Functions.Operation.ExecuteIfProjectSelected(() => Functions.Extract.ExtractAllFiles(true)));
                         Functions.Project.MakeProjectWithSavePath(true);
                         Functions.Extract.CopyGameFilesIndeterministic();
@@ -110,7 +109,7 @@ namespace rpg_patcher
                 }),
 
                 new MenuBarItem ("_Special", new MenuItem [] {
-                    new MenuItem ("_Check for RPG Maker installs in the registry", "", () => {
+                    new MenuItem ("Check for _RPG Maker installs in the registry", "", () => {
                         // $"RPG Maker XP installed: {(Functions.Checks.Installed.XP ? "Yes" : "No")}\nRPG Maker VX installed: {(Functions.Checks.Installed.VX ? "Yes" : "No")}\nRPG Maker VX Ace installed: {(Functions.Checks.Installed.VXAce ? "Yes" : "No")}"
                         Button closeDialog = new Button("Close", true);
                         closeDialog.Clicked += () => { StaticWindows.Main._window.SetFocus(); Application.RequestStop(); };
@@ -124,7 +123,7 @@ namespace rpg_patcher
                         Application.Run(installed);
                     }),
                     null,
-                    new MenuItem ("_RPG Maker 2000/03 Charset -> XP format (entire folder)", "", () => {
+                    new MenuItem ("RPG Maker 2000/03 Ch_arset -> XP format (entire folder)", "", () => {
                         string infile = "", outfile = "";
 
                         Functions.FileDialog.CreateOpenDialog("Navigate to folder of RPG Maker 2000/03 charsets.", "", new string[] {"png"}, () => { infile = Functions.FileDialog._OpenDialog.FilePath.ToString(); });
@@ -132,20 +131,13 @@ namespace rpg_patcher
 
                         Functions.Operation.RPGMaker2k.Convert2kCharsets(Path.GetDirectoryName(infile), Path.GetDirectoryName(outfile));
                     }),
-                    new MenuItem ("_RPG Maker 2000/03 Chipset -> XP tileset (entire folder)", "", () => {
+                    new MenuItem ("RPG Maker 2000/03 Ch_ipset -> XP tileset (entire folder)", "", () => {
                         string infile = "", outfile = "";
 
                         Functions.FileDialog.CreateOpenDialog("Navigate to folder of RPG Maker 2000/03 chipsets.", "", new string[] {"png"}, () => { infile = Functions.FileDialog._OpenDialog.FilePath.ToString(); });
                         Functions.FileDialog.CreateSaveDialog("Where to put the converted tilesets?", "", new string[] {"png"}, () => { outfile = Functions.FileDialog._SaveDialog.FilePath.ToString(); });
 
                         Functions.Operation.RPGMaker2k.Convert2kChipsets(Path.GetDirectoryName(infile), Path.GetDirectoryName(outfile));
-                    })
-                }),
-
-                new MenuBarItem ("_Settings", new MenuItem [] {
-                    new MenuItem ("_Open settings window", "", () => {
-                        StaticWindows.Settings settings = new StaticWindows.Settings();
-                        Application.Run(settings._window);
                     })
                 })
             });
